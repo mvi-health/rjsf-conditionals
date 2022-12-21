b146c3a2eed0fafe843a6eb9acd57edbe0ef8985
Shealyn Hindenlang <shealynntate@gmail.com>
2022-12-21 11:52:14 -0800

    Adding lib and dist files

---
+++ dist/cjs/env.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+const isDevelopment = () => {
+  return process.env.NODE_ENV !== "production";
+};
+var _default = {
+  isDevelopment
+};
+exports.default = _default;
